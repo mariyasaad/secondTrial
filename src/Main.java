e5b209858884d5010612b78345ec04561c347ab6
mariyasaad <mariya.saad3@gmail.com>
2025-10-01 02:42:08 +0300

    main branch commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello from main branch");
 
         Student student = new Student("maria","saad",25);
         student.print();
